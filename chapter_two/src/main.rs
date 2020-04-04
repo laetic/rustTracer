6517e4292c545c3eb30ecf525daf28e77d2e67a9
Richard <r.nguyen.206@gmail.com>
2020-04-04 13:05:14 -0600

    Redid ppm example with vec3 struct usage

--- chapter_two/src/main.rs
+++ chapter_two/src/main.rs
@@ -1,8 +1,11 @@
 mod vec3;
+use vec3::*;
+use std::fs::File;
+use std::io::prelude::*;
 
-fn main() {
-    let mut v1 = vec3::Vec3::new (1.0, 2.0, 3.0);
-    let v2 = vec3::Vec3::new (4.0, 5.0, 6.0);
+fn main() -> std::io::Result<()> {
+    let mut v1 = Vec3::new (1.0, 2.0, 3.0);
+    let v2 = Vec3::new (4.0, 5.0, 6.0);
     println!("{}", v1);
     println!("addition: {} + {} = {}", v1, v2, (v1 + v2) );
     println!("indexes for v1: {}, {}, {}", v1[0], v1[1], v1[2]);
@@ -26,4 +29,30 @@ fn main() {
 
     v1.make_unit_vector();
     println!("result: {}", v1);
+
+    println!("{} dot {} = {}", v1, v2, v1.dot(v2));
+
+    println!("{} cross {} = {}", v1, v2, v1.cross(v2));
+
+    let nx = 200;
+    let ny = 100;
+    let mut file = File::create ("ch2.ppm")?;
+    let first_line = format!("P3\n{} {}\n255\n", nx, ny);
+    file.write(first_line.as_bytes());
+
+    for j in (0..ny).rev() {
+        //file.write(format!("{}\n", j).as_bytes());
+        for i in 0..nx {
+            let vec = Vec3::new ( i as f32 / nx as f32 , j as f32 / ny as f32, 0.2 );
+            // let r = i as f32 / nx as f32;
+            // let g = j as f32 / ny as f32;
+            // let b = 0.2;
+            let ir = 255.99 * vec[0];
+            let ig = 255.99 * vec[1];
+            let ib = 255.99 * vec[2];
+            file.write(format!("{} {} {}\n", ir as i32, ig as i32, ib as i32).as_bytes());
+        }
+    }
+
+    Ok(())
 }
--- chapter_two/src/vec3.rs
+++ chapter_two/src/vec3.rs
@@ -27,6 +27,15 @@ impl Vec3 {
         let k = 1.0 / self.length();
         self.e[0] *= k; self.e[1] *= k; self.e[2] *= k;
     }
+    pub fn dot(&self, other:Vec3) -> f32 {
+        self.e[0] * other.e[0] + self.e[1] * other.e[1] + self.e[2] * other.e[2]
+    }
+
+    pub fn cross(&self, other:Vec3) -> Vec3 {
+        Vec3 { e: [ self.e[1] * other.e[2] - self.e[2] * other.e[1], 
+            -( self.e[0] * other.e[2] - self.e[2] * other.e[0] ), 
+            self.e[0] * other.e[1] - self.e[1] * other.e[0]]}
+    }
 }
 
 // https://doc.rust-lang.org/1.30.0/book/2018-edition/appendix-02-operators.html?highlight=op#operators
@@ -41,6 +50,35 @@ impl Sub for Vec3 {
     type Output = Vec3;
     fn sub(self, other:Vec3 ) -> Vec3 {
         Vec3 { e : [ self.e[0] - other.e[0], self.e[1] - other.e[1], self.e[2] - other.e[2] ]}
+    }
+}
+
+
+impl Mul for Vec3 {
+    type Output = Vec3;
+    fn mul(self, other:Vec3) -> Vec3 {
+        Vec3 { e: [ self.e[0] * other.e[0],  self.e[1] * other.e[1],  self.e[2] * other.e[2] ] }
+    }
+}
+
+impl Div for Vec3 {
+    type Output = Vec3;
+    fn div(self, other:Vec3) -> Vec3 {
+        Vec3 { e: [ self.e[0] / other.e[0],  self.e[1] / other.e[1],  self.e[2] / other.e[2] ] }
+    }
+}
+
+impl Mul<f32> for Vec3 {
+    type Output = Vec3;
+    fn mul(self, other:f32) -> Vec3 {
+        Vec3 { e: [ self.e[0] * other,  self.e[1] * other,  self.e[2] * other ] }
+    }
+}
+
+impl Div<f32> for Vec3 {
+    type Output = Vec3;
+    fn div(self, other:f32) -> Vec3 {
+        Vec3 { e: [ self.e[0] / other,  self.e[1] / other,  self.e[2] / other ] }
     }
 }
 
