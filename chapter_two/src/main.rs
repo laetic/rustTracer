5709c7a8ad18835997a2af321f815849867cf10b
Richard <r.nguyen.206@gmail.com>
2020-04-04 10:20:25 -0600

    Added my own vec3 struct

--- chapter_two/src/main.rs
+++ chapter_two/src/main.rs
@@ -1,8 +1,8 @@
-mod Vec3;
+mod vec3;
 
 fn main() {
-    let v1 = Vec3::Vec3::new (1.0, 2.0, 3.0);
-    let v2 = Vec3::Vec3 {e:[1.0,2.0,3.0]};
-
-    println!("{}", v1.to_string())
+    let v1 = vec3::Vec3::new (1.0, 2.0, 3.0);
+    let v2 = vec3::Vec3::new (4.0, 5.0, 6.0);
+    println!("{}", v1.to_string());
+    println!("addition: {} + {} = {}", v1.to_string(), v2.to_string(), (v1 + v2).to_string() );
 }
--- chapter_two/src/vec3.rs
+++ chapter_two/src/vec3.rs
@@ -1,3 +1,5 @@
+use std::ops::*;
+
 #[derive(Debug)]
 pub struct Vec3 {
     pub e : [f32; 3]
@@ -11,10 +13,33 @@ impl Vec3 {
     pub fn x(&self) -> f32 { self.e[0] }
     pub fn y(&self) -> f32 { self.e[1] }
     pub fn z(&self) -> f32 { self.e[2] }
-    // pub fn r(&self) -> f32 { self.e[0] }
-    // pub fn g(&self) -> f32 { self.e[1] }
-    // pub fn b(&self) -> f32 { self.e[2] }
+    pub fn r(&self) -> f32 { self.e[0] }
+    pub fn g(&self) -> f32 { self.e[1] }
+    pub fn b(&self) -> f32 { self.e[2] }
+
     pub fn to_string(&self) -> String {
-        format! ( "{} {} {}", self.x(), self.y(), self.z())
+        format! ( "({},{},{})", self.x(), self.y(), self.z())
     }
 }
+
+// https://doc.rust-lang.org/1.30.0/book/2018-edition/appendix-02-operators.html?highlight=op#operators
+impl Add for Vec3 {
+    type Output = Vec3;
+    fn add(self, other:Vec3 ) -> Vec3 {
+        Vec3 { e : [ self.e[0] + other.e[0],  self.e[1] + other.e[1],  self.e[2] + other.e[2] ]}
+    }
+}
+
+impl Sub for Vec3 {
+    type Output = Vec3;
+    fn sub(self, other:Vec3 ) -> Vec3 {
+        Vec3 { e : [ self.e[0] - other.e[0], self.e[1] - other.e[1], self.e[2] - other.e[2] ]}
+    }
+}
+
+// impl Index for Vec3 {
+//     type Output = f32;
+//     fn index(index: Idx) -> f32 {
+//         self.e[index]
+//     }
+// }
