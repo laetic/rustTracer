c8450f46d8f4f55d396780a2382fdf3b2d9826b1
Richard <r.nguyen.206@gmail.com>
2020-04-04 11:53:16 -0600

    Added a lot of op assigns

--- chapter_two/src/main.rs
+++ chapter_two/src/main.rs
@@ -1,9 +1,29 @@
 mod vec3;
 
 fn main() {
-    let v1 = vec3::Vec3::new (1.0, 2.0, 3.0);
+    let mut v1 = vec3::Vec3::new (1.0, 2.0, 3.0);
     let v2 = vec3::Vec3::new (4.0, 5.0, 6.0);
     println!("{}", v1);
     println!("addition: {} + {} = {}", v1, v2, (v1 + v2) );
     println!("indexes for v1: {}, {}, {}", v1[0], v1[1], v1[2]);
+    
+    println!("add assign: {}", v1);
+    let v3 = v1 + v2;
+    println!("adding v3 {} to v1", v3);
+    v1 += v3;
+    println!("result: {}", v1);
+
+    v1 -= v3;
+    println!("result: {}", v1);
+
+    v1 *= 2.0;
+    println!("result: {}", v1);
+
+    v1 *= v2;
+    println!("result: {}", v1);
+
+    println!("length: {}", v1.length());
+
+    v1.make_unit_vector();
+    println!("result: {}", v1);
 }
--- chapter_two/src/vec3.rs
+++ chapter_two/src/vec3.rs
@@ -21,6 +21,12 @@ impl Vec3 {
     // pub fn to_string(&self) -> String {
     //     format! ( "({},{},{})", self.x(), self.y(), self.z())
     // }
+    pub fn length(&self) -> f32 { (self.x()*self.x() + self.y()*self.y() + self.z()*self.z()).sqrt()}
+    pub fn squared_length(&self) -> f32 {(self.x()*self.x() + self.y()*self.y() + self.z()*self.z())}
+    pub fn make_unit_vector(&mut self) {
+        let k = 1.0 / self.length();
+        self.e[0] *= k; self.e[1] *= k; self.e[2] *= k;
+    }
 }
 
 // https://doc.rust-lang.org/1.30.0/book/2018-edition/appendix-02-operators.html?highlight=op#operators
@@ -42,6 +48,54 @@ impl Index<usize> for Vec3 {
     type Output = f32;
     fn index(&self, index: usize) -> &f32 {
         &self.e[index]
+    }
+}
+
+impl AddAssign for Vec3 {
+    fn add_assign(&mut self, other: Self) {
+        *self = Self {
+            e: [ self.e[0] + other.e[0],  self.e[1] + other.e[1],  self.e[2] + other.e[2] ]
+        }
+    }
+}
+
+impl SubAssign for Vec3 {
+    fn sub_assign(&mut self, other: Self) {
+        *self = Self {
+            e: [ self.e[0] - other.e[0],  self.e[1] - other.e[1],  self.e[2] - other.e[2] ]
+        }
+    }
+}
+
+impl MulAssign for Vec3 {
+    fn mul_assign(&mut self, other: Self) {
+        *self = Self {
+            e: [ self.e[0] * other.e[0],  self.e[1] * other.e[1],  self.e[2] * other.e[2] ]
+        }
+    }
+}
+
+impl MulAssign<f32> for Vec3 {
+    fn mul_assign(&mut self, other:f32) {
+        *self = Self {
+            e: [ self.e[0] * other ,  self.e[1] * other ,  self.e[2] * other  ]
+        }
+    }
+}
+
+impl DivAssign for Vec3 {
+    fn div_assign(&mut self, other: Self) {
+        *self = Self {
+            e: [ self.e[0] / other.e[0],  self.e[1] / other.e[1],  self.e[2] / other.e[2] ]
+        }
+    }
+}
+
+impl DivAssign<f32> for Vec3 {
+    fn div_assign(&mut self, other:f32) {
+        *self = Self {
+            e: [ self.e[0] / other ,  self.e[1] / other ,  self.e[2] / other  ]
+        }
     }
 }
 
